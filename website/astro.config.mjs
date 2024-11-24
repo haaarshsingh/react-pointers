79ed713bbac3ab5e743ac0c56502073bd116189f
Harsh Singh <hi.harsh@protonmail.ch>
2024-11-24 04:08:29 -0600

    finish website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+import sectionize from "remark-sectionize";
 
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), mdx()],
+  markdown: {
+    remarkPlugins: [sectionize],
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
 });
