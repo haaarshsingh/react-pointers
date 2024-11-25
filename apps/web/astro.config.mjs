1c335103c7ca8d861e1491c6adc4183c79c4aff1
Harsh Singh <hi.harsh@protonmail.ch>
2024-11-24 23:49:02 -0600

    add turborepo

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sectionize from "remark-sectionize";
 
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     remarkPlugins: [sectionize],
     shikiConfig: {
--- src/index.tsx
+++
@@ -1,2 +0,0 @@
-const e = 1;
-export default e;
